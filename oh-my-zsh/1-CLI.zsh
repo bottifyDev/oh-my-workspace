b6e37de63d91fb2439df73e57f4382ad219d5c1c
bottifyDev <bottify@ya.ru>
2022-04-04 10:32:51 +1000

    fast commit

--- oh-my-zsh/1-CLI.zsh
+++ oh-my-zsh/1-CLI.zsh
@@ -4,6 +4,7 @@
 alias oh="cd $OMW && clear && help"
 alias oh-reload="oh-zsh-reload"
 alias help="clear && cat $OMW_DOCS_HELP"
+alias work="cd ~/projects && clear && cat $OMW/docs/work.md && ls" 
 
 # OH MY ZSH COMANDS
 alias oh-zsh="oh-zsh-help && cd $ZSH_CUSTOM"
